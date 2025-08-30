ae76b9eb0542d9faad57e56df63b448b563cf2f4
dkharyanto <32188167+dkharyanto@users.noreply.github.com>
2025-08-30 15:47:04 +0700

    Update GaGS.lua

--- GaGS.lua
+++ GaGS.lua
@@ -4,7 +4,7 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local scripts = {
-    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/dkharyanto/dodoy/refs/heads/main/v1.lua",
 }
 
 local url = scripts[game.PlaceId]
